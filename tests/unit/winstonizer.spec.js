ce2995bde2207fcbf60bf6716a1580b3b8565372
Kieran Goodary <kieran@2030.io>
2018-08-22 17:46:46 +0100

    feature/initial - WIP

--- tests/unit/winstonizer.spec.js
+++ tests/unit/winstonizer.spec.js
@@ -1,7 +1,61 @@
+const winston = require('winston');
 const winstonizer = require('../../lib/winstonizer');
+
+// jest.mock('winston');
+// jest.mock('../../lib/winstonizer');
 
 describe('The Winstonizer Module', () => {
   describe('construct function', () => {
-    //
+    it('should have called winston.createLogger function', () => {
+      const derivedLogger = winstonizer.build();
+    });
+  });
+
+  describe('constructTransports function', () => {
+    it('should throw an error if no array passed into the first parameter', () => {
+      expect(() => {
+        winstonizer.constructTransports();
+      }).toThrowError();
+    });
+
+    it('should throw an error if an empty array passed into the first parameter', () => {
+      expect(() => {
+        winstonizer.constructTransports([]);
+      }).toThrowError();
+    });
+
+    it('should NOT throw an error if array passed into the first parameter', () => {
+      expect(() => {
+        winstonizer.constructTransports([
+          {
+            type: 'Console',
+          },
+          {
+            type: 'File',
+          },
+        ]);
+      }).not.toThrowError();
+    });
+
+    it('should return a Winston Console transport when "console" specified', () => {
+      const transports = winstonizer.constructTransports([
+        {
+          type: 'Console',
+          options: {},
+        },
+      ]);
+      const transportToAssert = JSON.stringify(transports[0]);
+      const transportExpected = JSON.stringify(
+        new winston.transports.Console(),
+      );
+
+      expect(transportToAssert).toEqual(transportExpected);
+    });
+
+    it('should return just an empty array if an array of empty objects specified', () => {
+      const transports = winstonizer.constructTransports([{}, {}, {}]);
+
+      expect(transports).toEqual([]);
+    });
   });
 });
